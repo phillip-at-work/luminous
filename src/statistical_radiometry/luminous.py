278a0b3f26756b254a793e0656f8bd3d014e010a
phillipcurtsmith <phillip.curtsmith@gmail.com>
2024-03-19 21:09:37 -0400

    added mul, #compute_uw_per_nm

--- src/statistical_radiometry/luminous.py
+++ src/statistical_radiometry/luminous.py
@@ -1,19 +1,28 @@
+from __future__ import annotations
 import numpy as np
 from typing import List, Union, Optional
 
+
+ELECTRON_CHARGE = 1.602e-19
+
+
 class Spectrum:
-    def __init__(self, wavelengths: Union[np.ndarray, List[float]], amplitudes: Union[np.ndarray, List[float]]):
+    def __init__(
+        self,
+        wavelengths: Union[np.ndarray, List[float]],
+        amplitudes: Union[np.ndarray, List[float]],
+    ):
         """
         Create a Spectrum object. Radiometrc and photometric calculations assume amplitudes data in units of [uW/cm^2/nm] (absolute spectral irradiance).
         NOTE: method comments to follow assume standard units of uW/cm^2/nm.
-        
+
         Parameters:
         wavelengths (list or np.array): abscissa values for spectrum amplitudes
         amplitudes (list or np.array): spectrum data
         """
         if len(wavelengths) != len(amplitudes):
             raise SystemError("Length of wavelength and amplitude vectors must match.")
-        
+
         if not isinstance(wavelengths, np.ndarray):
             self.wavelengths = np.array(wavelengths, float)
         else:
@@ -22,8 +31,10 @@ class Spectrum:
         positive = np.all(self.wavelengths > 0)
         sorted_ascending = (np.diff(self.wavelengths) >= 0).all()
         if not positive or not sorted_ascending:
-            raise SystemError("Spectral wavelengths must be a vector of values in ascending order, where each value is greater than 0.")
-        
+            raise SystemError(
+                "Spectral wavelengths must be a vector of values in ascending order, where each value is greater than 0."
+            )
+
         if self.wavelengths.size != np.unique(self.wavelengths).size:
             raise SystemError("All wavelength values must be unique.")
 
@@ -33,11 +44,13 @@ class Spectrum:
             self.amplitudes = amplitudes
 
         if not np.isrealobj(self.wavelengths) and np.isrealobj(self.amplitudes):
-            raise TypeError("Spectral wavelength and amplitude arrays must be numeric only.")
+            raise TypeError(
+                "Spectral wavelength and amplitude arrays must be numeric only."
+            )
 
     def __len__(self) -> int:
         return self.wavelengths.size
-    
+
     def __getitem__(self, index) -> tuple:
         return (self.wavelengths[index], self.amplitudes[index])
 
@@ -45,10 +58,26 @@ class Spectrum:
         if len(self) == 1:
             return f"Spectrum(wavelength={self.wavelengths[0]}, amplitude={self.amplitudes[0]})"
         return f"Spectrum(wavelengths={self.wavelengths[0]}... {self.wavelengths[-1]}, amplitudes={self.amplitudes[0]}... {self.amplitudes[-1]})"
-    
-    # TODO add appropriate dunder to slice spectral object, e.g., spectrum[i:k] slices amplitude and wavelength indices appropriately
 
-    def wavelength_indices(self, lower_wavelength, upper_wavelength) -> tuple[float, float]:
+    def __mul__(self, s: Union[Spectrum, float, int]) -> Spectrum:
+        if isinstance(s, Spectrum):
+            if not np.array_equal(self.wavelengths, s.wavelengths):
+                raise ValueError("Spectra must share an abscissa to be multiplied.")
+            return Spectrum(
+                self.wavelengths, np.multiply(self.amplitudes, s.amplitudes)
+            )
+        if isinstance(s, float) or isinstance(s, int):
+            return Spectrum(self.wavelengths, self.amplitudes * s)
+        raise TypeError(
+            "Spectrum multiplication requires Spectrum objects, or a Spectrum and numeric type."
+        )
+
+    # TODO add appropriate dunder to slice spectral object, e.g., spectrum[i:k] slices amplitude and wavelength indices appropriately
+    # TODO operator overloading for whole vector arithmetic?
+
+    def wavelength_indices(
+        self, lower_wavelength, upper_wavelength
+    ) -> tuple[float, float]:
         """
         Returns indice(s) associated with wavelength pair.
 
@@ -57,10 +86,17 @@ class Spectrum:
         upper_wavelength: the ending point of the wavelength interval
         """
         if lower_wavelength >= upper_wavelength:
-            raise IndexError("Lower wavelength argument must not be equal or larger than upper.")
-        return (np.where(self.wavelengths == lower_wavelength)[0][0], np.where(self.wavelengths == upper_wavelength)[0][0])
-    
-    def peak_amplitudes(self, interval=[0,np.Inf], return_as_list=False, indices=False) -> Union[np.ndarray, List[float]]:
+            raise IndexError(
+                "Lower wavelength argument must not be equal or larger than upper."
+            )
+        return (
+            np.where(self.wavelengths == lower_wavelength)[0][0],
+            np.where(self.wavelengths == upper_wavelength)[0][0],
+        )
+
+    def peak_amplitudes(
+        self, interval=[0, np.Inf], return_as_list=False, indices=False
+    ) -> Union[np.ndarray, List[float]]:
         """
         Find peak amplitudes in spectral data.
 
@@ -79,7 +115,7 @@ class Spectrum:
             raise ValueError("Input data contains NaN or Inf values.")
         if interval[1] <= interval[0]:
             raise ValueError("Second element must be greater than the first element.")
-    
+
         peaks, _ = find_peaks(self.amplitudes, height=interval)
         if return_as_list:
             if indices:
@@ -89,7 +125,9 @@ class Spectrum:
             return peaks
         return self.amplitudes[peaks]
 
-    def peak_indices(self, interval=[0,np.Inf], return_as_list=False) -> Union[np.ndarray, List[int]]:
+    def peak_indices(
+        self, interval=[0, np.Inf], return_as_list=False
+    ) -> Union[np.ndarray, List[int]]:
         """
         Find peak indices in spectral data.
         See #peak_amplitudes
@@ -111,13 +149,18 @@ class Spectrum:
         PeakPropertyWarning
             If `v` is not actually a peak index. To guard against this, pass index values returned from #peak_indices.
         """
-        if not 0 <= v <= len(self.amplitudes)-1:
-            raise IndexError(f"Index {v} does not exist! Param v must be an index between [0, {len(self.wavelengths)-1}].")
+        if not 0 <= v <= len(self.amplitudes) - 1:
+            raise IndexError(
+                f"Index {v} does not exist! Param v must be an index between [0, {len(self.wavelengths)-1}]."
+            )
         if not isinstance(v, (int, np.integer)):
-            raise TypeError("Param v must be an integer index associated with a peak in amplitudes data.")
-        
+            raise TypeError(
+                "Param v must be an integer index associated with a peak in amplitudes data."
+            )
+
         from scipy.signal import peak_widths
-        p = peak_widths(self.amplitudes, peaks=[v], rel_height=0.5)    
+
+        p = peak_widths(self.amplitudes, peaks=[v], rel_height=0.5)
         left = round(p[2][0])
         right = round(p[3][0])
         return self.wavelengths[right] - self.wavelengths[left]
@@ -144,40 +187,53 @@ class Spectrum:
                 count = 0
         return False
 
-    def compute_microwatts_per_sq_cm(self, lower_bound: Optional[float] = None, upper_bound: Optional[float] = None) -> float:
-        '''
+    def compute_microwatts_per_sq_cm(
+        self, lower_bound: Optional[float] = None, upper_bound: Optional[float] = None
+    ) -> float:
+        """
         Compute irradiance in [uW/cm^2]. Calculation assumes spectral data in units of [uW/cm^2/nm] (absolute spectral irradiance).
 
         Parameters:
         lower_bound (float): lower integration bound in abscissa units.
             Default is smallest amplitude value (0th index).
         upper_bound (float): upper integration bound in abscissa units.
-            NOTE: lower_bound arguments which do not exactly match an abscissa value are rounded up to the nearest value. 
+            NOTE: lower_bound arguments which do not exactly match an abscissa value are rounded up to the nearest value.
             E.g., only those abscissa values greater than or equal to the specified lower_bound are included in the integration interval.
             The same is true for upper_bound; only those abscissa values less than the upper_bound are included in the interval.
             Default is largest amplitude value (-1st index).
 
         Returns:
             float: irradiance
-        '''
+        """
         lower_bound = self.wavelengths[0] if lower_bound is None else lower_bound
         upper_bound = self.wavelengths[-1] if upper_bound is None else upper_bound
 
         if lower_bound < self.wavelengths[0]:
-            raise IndexError("lower_bound is out of bounds. Specified value is less than smallest amplitude value.")
+            raise IndexError(
+                "lower_bound is out of bounds. Specified value is less than smallest amplitude value."
+            )
         if upper_bound > self.wavelengths[-1]:
-            raise IndexError("upper_bound out of bounds. Specified value is greater than the maximum amplitude value.")
+            raise IndexError(
+                "upper_bound out of bounds. Specified value is greater than the maximum amplitude value."
+            )
         if lower_bound >= upper_bound:
-            raise IndexError("Lower wavelength argument must not be equal or larger than upper.")
-        
+            raise IndexError(
+                "Lower wavelength argument must not be equal or larger than upper."
+            )
+
         mask = (self.wavelengths >= lower_bound) & (self.wavelengths <= upper_bound)
         x = self.wavelengths[mask]
         y = self.amplitudes[mask]
 
         return np.trapz(y=y, x=x)
-    
-    def compute_microwatts(self, collection_area: float, lower_bound: Optional[float] = None, upper_bound: Optional[float] = None) -> float:
-        '''
+
+    def compute_microwatts(
+        self,
+        collection_area: float,
+        lower_bound: Optional[float] = None,
+        upper_bound: Optional[float] = None,
+    ) -> float:
+        """
         Compute absolute power in uW.
 
         Parameters:
@@ -186,22 +242,184 @@ class Spectrum:
 
         Returns:
             float: absolute power in uW.
-        '''
-        return self.compute_microwatts_per_sq_cm(lower_bound=lower_bound, upper_bound=upper_bound) * collection_area
+        """
+        if collection_area < 0:
+            raise ValueError("Collection area cannot be less than 0 cm^2.")
+        return (
+            self.compute_microwatts_per_sq_cm(
+                lower_bound=lower_bound, upper_bound=upper_bound
+            )
+            * collection_area
+        )
+
+    def compute_microwatts_per_nm(self, collection_area: Union[float, int]) -> Spectrum:
+        """
+        Compute spectral power in uW/nm.
+
+        Parameters:
+        collection_area: see #compute_microwatts
+
+        Returns:
+            float: spectral power in uW/nm
+        """
+        return self * collection_area
+
+    def compute_joules(
+        self,
+        collection_area: float,
+        integration_time: float,
+        lower_bound: Optional[float] = None,
+        upper_bound: Optional[float] = None,
+    ) -> float:
+        """
+        Compute energy in Joules.
+
+        Parameters:
+        integration_time: temporal integration period in seconds.
+        collection_area, lower_bound, upper_bound: see #compute_microwatts
+
+        Returns:
+            float: total energy in Joules.
+        """
+        if integration_time < 0:
+            raise ValueError("Integration time cannot be less than 0s.")
+        return (
+            self.compute_microwatts(
+                lower_bound=lower_bound,
+                upper_bound=upper_bound,
+                collection_area=collection_area,
+            )
+            * integration_time
+            * 1e-6
+        )
+
+    def compute_electron_volts(
+        self,
+        collection_area: float,
+        integration_time: float,
+        lower_bound: Optional[float] = None,
+        upper_bound: Optional[float] = None,
+    ) -> float:
+        """
+        Compute energy in electron volts.
+
+        Parameters:
+        See #compute_joules.
+
+        Returns:
+            float: total energy in electron volts.
+        """
+        return self.compute_joules(
+            lower_bound=lower_bound,
+            upper_bound=upper_bound,
+            collection_area=collection_area,
+            integration_time=integration_time,
+        ) * (1 / ELECTRON_CHARGE)
+
+    def compute_lux(
+        self, luminous_efficiency: Spectrum, max_luminous_efficiency_coefficient: float
+    ) -> float:
+        pass
+
+
+#     /**
+#  * Compute lux in [lumen/m^2] for an arbitrary luminousEfficiencyFunction. Calculation assumes #getSpectralData in units of [uW/cm^2/nm] (absolute spectral irradiance).
+#  * Calculation replicates Ocean Optics Spectral Processing and Math AdvancedPhotometrics#computeIlluminanceLux.
+#  * Note: user need not specify an incident area in m^2, as method returns lux in lumens/m^2 based upon the collection area in cm^2 for original data.
+#  * Note: #getSpectralWavelengths must match efficiencyFunctionWavelengths. Else, #crop.
+# */
+# double Spectrum::computeLux(    Spectrum & luminousEfficiencyFunction,
+#                                 double & maxLuminousEfficiencyCoefficient ) {
+#     double convertMicrowattToWatt = 1.0 / 1000000.0;
+#     double convertPerCmSquaredToPerMeterSquared = 1.0 / 10000.0;
+#     Spectrum wattsPerMeterSquaredPerNanometer = *this * convertMicrowattToWatt * convertPerCmSquaredToPerMeterSquared;
+#     Spectrum lumensPerMeterSquaredPerNanometer = wattsPerMeterSquaredPerNanometer *= luminousEfficiencyFunction;
+#     return maxLuminousEfficiencyCoefficient * lumensPerMeterSquaredPerNanometer.computeIntegral( lowerBound, upperBound );
+# }
+
+# /**
+#  * Compute photopic lux in [lumen/m^2]. Calculation assumes #getSpectralData in units of [uW/cm^2/nm] (absolute spectral irradiance).
+#  * See base method #computeLux.
+# */
+# double Spectrum::computeLuxPhotopic( ) {
+#     Spectrum photopic { photopicEfficiencyData, efficiencyFunctionWavelengths };
+#     return computeLux( photopic, peakPhotopicLuminosity );
+# }
+
+# /**
+#  * Compute scotopic lux in [lumen/m^2]. Calculation assumes #getSpectralData in units of [uW/cm^2/nm] (absolute spectral irradiance).
+#  * See base method #computeLux.
+# */
+# double Spectrum::computeLuxScotopic( ) {
+#     Spectrum scotopic { scotopicEfficiencyData, efficiencyFunctionWavelengths };
+#     return computeLux( scotopic, peakScotopicLuminosity );
+# }
+
+# /**
+#  * Compute lumens. Calculation assumes #getSpectralData in units of [uW/cm^2/nm] (absolute spectral irradiance).
+#  * collectionArea in [cm^2].
+# */
+# double Spectrum::computeLumens( Spectrum & luminousEfficiencyFunction,
+#                                 double & maxLuminousEfficiencyCoefficient,
+#                                 double & collectionArea ) {
+#     double convertPerMeterSquaredToPerCentimeterSquared = 10000.0;
+#     return collectionArea * convertPerMeterSquaredToPerCentimeterSquared * computeLux( luminousEfficiencyFunction, maxLuminousEfficiencyCoefficient );
+# }
+
+# /**
+#  * Compute candella in [lumen/steradians]. Calculation assumes #getSpectralData in units of [uW/cm^2/nm] (absolute spectral irradiance).
+#  * Calculation replicates Ocean Optics Spectral Processing and Math AdvancedPhotometrics#computeLuminousIntensityCandela.
+#  * solidAngle in [steradians]. collectionArea in [cm^2].
+# */
+# double Spectrum::computeCandella(   Spectrum & luminousEfficiencyFunction,
+#                                     double & maxLuminousEfficiencyCoefficient,
+#                                     double & collectionArea,
+#                                     double & solidAngle ) {
+#     return computeLumens( luminousEfficiencyFunction, maxLuminousEfficiencyCoefficient, collectionArea ) / solidAngle;
+# }
+
+# /**
+#  * Compute luminous flux in [photons/cm^2/s]. Calculation assumes #getSpectralData in units of [uW/cm^2/nm] (absolute spectral irradiance).
+#  * Calculation replicates Ocean Optics Spectral Processing and Math AdvancedPhotometrics#computePhotonsPerCmSquaredPerSecond.
+# */
+# double Spectrum::computePhotonsPerCmSquaredPerSecond( int & lowerBoundWavelength, int & upperBoundWavelength ) {
+#     std::vector<double> photonsPerCmSquaredPerSecondPerNanometer = {};
+#     double plancksConstant = 6.62607015e-34;
+#     double velocityOfLight = 2.998e8;
+#     double metersToNanometer = 1e-9;
+#     double wattsToMicrowatts = 1e-6;
+#     for ( int i = 0; i < getSpectralData().size(); i++ ) {
+#         photonsPerCmSquaredPerSecondPerNanometer.push_back( (getSpectralData().at(i) * getSpectralWavelengths().at(i) * metersToNanometer * wattsToMicrowatts) / ( plancksConstant * velocityOfLight ) );
+#     }
+#     return Spectrum{ photonsPerCmSquaredPerSecondPerNanometer, getSpectralWavelengths() }.computeIntegral( lowerBound, upperBound );
+# }
+
+# /**
+#  * Compute total photons. Calculation assumes #getSpectralData in units of [uW/cm^2/nm] (absolute spectral irradiance).
+#  * Calculation replicates Ocean Optics Spectral Processing and Math AdvancedPhotometrics#computeTotalPhotons.
+#  * integrationTime in [seconds]. collectionArea in [cm^2].
+# */
+# double Spectrum::computeTotalPhotons( int & lowerBoundWavelength, int & upperBoundWavelength, double & collectionArea, double & integrationTime ) {
+#     return computePhotonsPerCmSquaredPerSecond( lowerBoundWavelength, upperBoundWavelength ) * collectionArea * integrationTime;
+# }
+
 
 #
 # test and temp content
 #
 
+
 def plot(s: Spectrum):
     import matplotlib.pyplot as plt
+
     plt.plot(s.wavelengths, s.amplitudes)
     plt.show()
 
+
 # amplitudes = [0.027, 0.082, 0.082, 0.27, 0.826, 2.732, 7.923, 15.709, 25.954, 40.98, 62.139, 94.951, 142.078, 220.609, 303.464, 343.549, 484.93, 588.746, 651.582, 679.551, 683, 679.585, 650.216, 594.21, 517.031, 430.973, 343.549, 260.223, 180.995, 119.525, 73.081, 41.663, 21.856, 11.611, 5.607, 2.802, 1.428, 0.715, 0.355, 0.17, 0.082, 0.041, 0.02]
 # wavelengths = [380, 390, 391, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 507, 510, 520, 530, 540, 550, 555, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 760, 770]
-wavelengths = np.linspace(1/1000, np.pi, 1000)
-amplitudes = np.sin(20*wavelengths)*np.sin(wavelengths)
+wavelengths = np.linspace(1 / 1000, np.pi, 1000)
+amplitudes = np.sin(20 * wavelengths) * np.sin(wavelengths)
 # amplitudes[34] = .8
 # amplitudes[35] = .8
 # amplitudes[36] = .8
@@ -210,11 +428,12 @@ s = Spectrum(wavelengths, amplitudes)
 print("String of 0th index: " + str(s[0]))
 print("Length: " + str(len(s)))
 print("String of object: " + str(s))
-p = s.peak_indices([0.5,1])
+p = s.peak_indices([0.5, 1])
 print("Peaks: " + str(p))
 print(f"0th peak index={p[0]}, value={amplitudes[p[0]]}")
 print("FWHM: " + str(s.full_width_half_max(p[0])))
 print("saturated? " + str(s.is_saturated()))
-print("irradiance: " + str(s.microwatt_per_sq_cm(lower_bound=1.2, upper_bound=3.16)))
-plot(s)
-
+print(
+    "irradiance: " + str(s.compute_microwatts_per_sq_cm(lower_bound=1.2, upper_bound=3))
+)
+plot(s * 3.13)

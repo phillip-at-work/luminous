c9067f7f4026bdd4c6b72544c1fc98ff96444eb3
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-01-04 21:21:49 -0500

    updates to ConcreteRayDebugger prints obj file with color

--- src/utilities/ray_debugger.py
+++ src/utilities/ray_debugger.py
@@ -11,16 +11,17 @@ class NullRayDebugger(RayDebugger):
     No-operation RayDebugger.
     '''
 
-    def add_vector(self, start_point, end_point, color):
+    def add_vector(self, end_point, start_point=(0,0,0), color=(0,0,0)):
         pass
-    def add_point(self, end_point, color):
+    def add_point(self, end_point, color=(0,0,0)):
         pass
     def plot(self):
         pass
 
-class ConcreteRayDebugger(RayDebugger):
+class ConcreteRayDebugger:
     '''
     Actual RayDebugger compiles data and plots rays.
+    Prints .obj, .mtl to specified directory.
     '''
 
     def __init__(self, shaft_radius=0.05, head_radius=0.1, head_length=0.2, point_radius=None):
@@ -33,16 +34,15 @@ class ConcreteRayDebugger(RayDebugger):
         self.renderer = vtk.vtkRenderer()
         self.vectors = list()
         self.points = list()
+        self.luminous_dialog_title = "luminous ray debugger"
 
-    def add_vector(self, start_point, end_point, color):
+    def add_vector(self, end_point, start_point=(0,0,0), color=(0,0,0)):
         self.vectors.append((start_point, end_point, color))
 
-    def add_point(self, end_point, color):
+    def add_point(self, end_point, color=(0,0,0)):
         self.points.append((end_point, color))
 
     def create_arrow(self, start_point, end_point, color):
-        
-        # vector direction and length
         vector = [end_point[i] - start_point[i] for i in range(3)]
         length = self.vtk.vtkMath.Norm(vector)
         self.vtk.vtkMath.Normalize(vector)
@@ -57,71 +57,67 @@ class ConcreteRayDebugger(RayDebugger):
 
         cross_vec = [0.0, 0.0, 0.0]
         self.vtk.vtkMath.Cross([1, 0, 0], vector, cross_vec)
-        angle = self.vtk.vtkMath.DegreesFromRadians(vtk.vtkMath.AngleBetweenVectors([1, 0, 0], vector))
+        angle = self.vtk.vtkMath.DegreesFromRadians(self.vtk.vtkMath.AngleBetweenVectors([1, 0, 0], vector))
         transform.RotateWXYZ(angle, cross_vec)
 
         transform.Scale(length, 1, 1)
 
-        mapper = self.vtk.vtkPolyDataMapper()
-        actor = self.vtk.vtkActor()
         transform_filter = self.vtk.vtkTransformPolyDataFilter()
         transform_filter.SetTransform(transform)
         transform_filter.SetInputConnection(arrow_source.GetOutputPort())
         transform_filter.Update()
 
+        mapper = self.vtk.vtkPolyDataMapper()
         mapper.SetInputConnection(transform_filter.GetOutputPort())
+
+        actor = self.vtk.vtkActor()
         actor.SetMapper(mapper)
         actor.GetProperty().SetColor(color)
 
         return actor
 
     def create_sphere(self, center, radius, color):
-
         sphere_source = self.vtk.vtkSphereSource()
         sphere_source.SetCenter(center)
         sphere_source.SetRadius(radius)
         sphere_source.SetPhiResolution(16)
         sphere_source.SetThetaResolution(16)
+        sphere_source.Update()
 
         mapper = self.vtk.vtkPolyDataMapper()
-        actor = self.vtk.vtkActor()
         mapper.SetInputConnection(sphere_source.GetOutputPort())
+
+        actor = self.vtk.vtkActor()
         actor.SetMapper(mapper)
         actor.GetProperty().SetColor(color)
 
         return actor
-    
+
     def plot(self, path="./results", filename="debug_ray_trace", display_3d_plot=False):
-        # Ensure the directory exists
         os.makedirs(path, exist_ok=True)
-        full_path = os.path.join(path, filename + ".ply")
-
-        # Create an append filter to combine all geometry
-        append_filter = self.vtk.vtkAppendPolyData()
-
-        for start_point, end_point, color in self.vectors:
-            arrow_actor = self.create_arrow(start_point, end_point, color)
-            append_filter.AddInputData(arrow_actor.GetMapper().GetInput())
-
-        for center, color in self.points:
-            sphere_actor = self.create_sphere(center, self.point_radius, color)
-            append_filter.AddInputData(sphere_actor.GetMapper().GetInput())
-
-        append_filter.Update()
-
-        # ply file write
-        ply_writer = self.vtk.vtkPLYWriter()
-        ply_writer.SetFileName(full_path)
-        ply_writer.SetInputConnection(append_filter.GetOutputPort())
-        ply_writer.SetColorModeToDefault()  # This mode attempts to save color information if present
-        ply_writer.Write()
+        full_path = os.path.join(path, filename)
 
         render_window = self.vtk.vtkRenderWindow()
         render_window.AddRenderer(self.renderer)
 
-        # display dialog with 3d volume
-        if display_3d_plot:
+        assembly = self.vtk.vtkAssembly()
+        for start_point, end_point, color in self.vectors:
+            arrow_actor = self.create_arrow(start_point, end_point, color)
+            assembly.AddPart(arrow_actor)
 
+        for center, color in self.points:
+            sphere_actor = self.create_sphere(center, self.point_radius, color)
+            assembly.AddPart(sphere_actor)
+
+        self.renderer.AddActor(assembly)
+
+        # export obj file with associated mtl
+        obj_exporter = self.vtk.vtkOBJExporter()
+        obj_exporter.SetFilePrefix(full_path)
+        obj_exporter.SetRenderWindow(render_window)
+        obj_exporter.Write()
+
+        if display_3d_plot:
             for start_point, end_point, color in self.vectors:
                 arrow_actor = self.create_arrow(start_point, end_point, color)
                 self.renderer.AddActor(arrow_actor)
@@ -167,13 +163,19 @@ class ConcreteRayDebugger(RayDebugger):
             self.renderer.AddActor(cube_axes)
             self.renderer.SetBackground(1, 1, 1)
 
+            render_window = self.vtk.vtkRenderWindow()
+            render_window.AddRenderer(self.renderer)
+
             render_window_interactor = self.vtk.vtkRenderWindowInteractor()
             render_window_interactor.SetRenderWindow(render_window)
-            render_window.Render()
+
             render_window_interactor.Initialize()
+            render_window.SetWindowName(self.luminous_dialog_title)
+
+            render_window.Render()
             render_window_interactor.Start()
 
         else:
-            # perform headless render
+
             render_window.SetOffScreenRendering(1)
             render_window.Render()

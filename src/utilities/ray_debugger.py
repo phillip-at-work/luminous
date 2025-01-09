4b7787e2bf68feafe175777d30ff809da84be2a4
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-01-08 21:14:41 -0500

    removed comments

--- src/utilities/ray_debugger.py
+++ src/utilities/ray_debugger.py
@@ -34,45 +34,21 @@ class ConcreteRayDebugger(RayDebugger):
         self.renderer = vtk.vtkRenderer()
         self.points = vtk.vtkPoints()
         self.vectors = vtk.vtkCellArray()
-        self.scalars = vtk.vtkFloatArray()  # Scalars for the lookup table
+        self.scalars = vtk.vtkFloatArray()
         self.lookup_table = vtk.vtkLookupTable()
         self.luminous_dialog_title = "luminous ray debugger"
 
-        # Initialize the lookup table
         self.lookup_table.SetNumberOfTableValues(256*256*256)
         self.lookup_table.Build()
 
-        # Fill the lookup table with colors
         for i in range(256*256*256):
             r = (i // (256*256)) % 256
             g = (i // 256) % 256
             b = i % 256
             self.lookup_table.SetTableValue(i, r/255.0, g/255.0, b/255.0, 1.0)
 
-    def add_vector(self, end_point, start_point=(0,0,0), color=(0,0,0)):
-        # Convert the RGB color to a scalar using a unique value for each color
-        color_scalar = color[0] * 256 * 256 + color[1] * 256 + color[2]
-
-        if isinstance(end_point, Vector):
-            end_point = end_point.components()
-
-        try:
-            for x_end, y_end, z_end, x_start, y_start, z_start in zip(*end_point, *start_point):
-                self._insert_vector(x_start, y_start, z_start, x_end, y_end, z_end, color_scalar)
-        except TypeError:
-            self._insert_vector(*start_point, *end_point, color_scalar)
-
-    def _insert_vector(self, x_start, y_start, z_start, x_end, y_end, z_end, color_scalar):
-        start_id = self.points.InsertNextPoint((x_start, y_start, z_start))
-        end_id = self.points.InsertNextPoint((x_end, y_end, z_end))
-        self.vectors.InsertNextCell(2)
-        self.vectors.InsertCellPoint(start_id)
-        self.vectors.InsertCellPoint(end_id)
-        self.scalars.InsertNextValue(color_scalar)
-        self.scalars.InsertNextValue(color_scalar)
-
     def add_point(self, end_point, color=(0,0,0)):
-        # Convert the RGB color to a scalar using a unique value for each color
+
         color_scalar = color[0] * 256 * 256 + color[1] * 256 + color[2]
 
         if isinstance(end_point, Vector):
@@ -96,26 +72,22 @@ class ConcreteRayDebugger(RayDebugger):
         render_window = self.vtk.vtkRenderWindow()
         render_window.AddRenderer(self.renderer)
 
-        # Create a single sphere source for all points
         sphere_source = self.vtk.vtkSphereSource()
         sphere_source.SetRadius(self.point_radius)
         sphere_source.SetPhiResolution(16)
         sphere_source.SetThetaResolution(16)
         sphere_source.Update()
 
-        # Create a polydata to store all points and scalars
         point_polydata = self.vtk.vtkPolyData()
         point_polydata.SetPoints(self.points)
         point_polydata.GetPointData().SetScalars(self.scalars)
 
-        # Use vtkGlyph3D to replicate the sphere geometry
         glyphs = self.vtk.vtkGlyph3D()
         glyphs.SetSourceConnection(sphere_source.GetOutputPort())
         glyphs.SetInputData(point_polydata)
         glyphs.ScalingOff()
         glyphs.Update()
 
-        # Create a mapper and actor for the glyphs
         glyph_mapper = self.vtk.vtkPolyDataMapper()
         glyph_mapper.SetInputConnection(glyphs.GetOutputPort())
         glyph_mapper.SetLookupTable(self.lookup_table)
@@ -126,11 +98,10 @@ class ConcreteRayDebugger(RayDebugger):
 
         self.renderer.AddActor(glyph_actor)
 
-        # Ensure the rendering pipeline is updated
         self.renderer.GetRenderWindow().Render()
 
         if display_3d_plot:
-            # Calculate bounds for the cube axes
+
             all_points = self.vtk.vtkPoints()
             for i in range(self.points.GetNumberOfPoints()):
                 all_points.InsertNextPoint(self.points.GetPoint(i))
@@ -140,7 +111,6 @@ class ConcreteRayDebugger(RayDebugger):
 
             bounds = polydata.GetBounds()
 
-            # Create cube axes
             cube_axes = self.vtk.vtkCubeAxesActor()
             cube_axes.SetBounds(bounds)
             cube_axes.SetCamera(self.renderer.GetActiveCamera())
@@ -178,7 +148,6 @@ class ConcreteRayDebugger(RayDebugger):
             render_window.SetOffScreenRendering(1)
             render_window.Render()
 
-        # Convert scalars to RGB colors and add as a separate array
         color_array = self.vtk.vtkUnsignedCharArray()
         color_array.SetNumberOfComponents(3)
         color_array.SetName("Colors")
@@ -189,13 +158,11 @@ class ConcreteRayDebugger(RayDebugger):
             b = scalar_value % 256
             color_array.InsertNextTuple3(r, g, b)
 
-        # Add the color array to the polydata
         point_polydata.GetPointData().AddArray(color_array)
 
-        # Write the polydata with colors to a PLY file
         ply_writer = self.vtk.vtkPLYWriter()
         ply_writer.SetFileName(full_path + ".ply")
         ply_writer.SetInputData(point_polydata)
-        ply_writer.SetArrayName("Colors")  # Specify the color array to use
-        ply_writer.SetColorModeToDefault()  # Use the color array for coloring
+        ply_writer.SetArrayName("Colors")
+        ply_writer.SetColorModeToDefault()
         ply_writer.Write()

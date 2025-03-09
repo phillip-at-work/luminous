4688449648827e48e8e1a369a70483352e43ce3c
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-03-08 21:33:02 -0500

    further flesh out detector abstract base class

--- src/detector/detector.py
+++ src/detector/detector.py
@@ -1,27 +1,11 @@
 from abc import ABC, abstractmethod
+import numpy as np
+from PIL import Image
 
 from ..math.vector import Vector
 
 
 class Detector(ABC):
-    @abstractmethod
-    def capture_data(self):
-        pass
-
-    @abstractmethod
-    def view_data(self):
-        pass
-
-
-class PowerMeter(Detector):
-    def capture_data(self):
-        raise NotImplementedError(f"Method not yet implementated in {self.__class__}")
-    
-    def view_data(self):
-        raise NotImplementedError(f"Method not yet implementated in {self.__class__}")
-
-
-class Imager(Detector):
     def __init__(self, width: int, height: int, position: Vector, pointing_direction: Vector):
         '''
         Parameters:
@@ -35,8 +19,41 @@ class Imager(Detector):
         self.height = height
         self.pointing_direction = pointing_direction.norm()
 
-    def capture_data(self):
+    @abstractmethod
+    def _capture_data(self):
+        '''
+        Compile data and iteratively realize the model unique to that detector
+        '''
+        pass
+
+    @abstractmethod
+    def view_data(self):
+        '''
+        Perform any final processing and return data to user
+        '''
+        pass
+
+
+class PowerMeter(Detector):
+    def __init__(self, width: int, height: int, position: Vector, pointing_direction: Vector):
+        super().__init__(width, height, position, pointing_direction)
+        
+    def _capture_data(self):
+        raise NotImplementedError(f"Method not yet implementated in {self.__class__}")
+    
+    def view_data(self):
+        raise NotImplementedError(f"Method not yet implementated in {self.__class__}")
+
+
+class Imager(Detector):
+    def __init__(self, width: int, height: int, position: Vector, pointing_direction: Vector):
+        super().__init__(width, height, position, pointing_direction)
+
+    def _capture_data(self):
         pass
 
     def view_data(self):
+        '''
+        Pixel data is a color
+        '''
         pass

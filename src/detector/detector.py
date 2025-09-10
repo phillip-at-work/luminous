bcad09e2d376b6307fedcc705dc028ddc3d4563b
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-09-10 19:56:18 -0400

    added blinn-phong citation, Detector

--- src/detector/detector.py
+++ src/detector/detector.py
@@ -57,26 +57,25 @@ class Camera(Detector):
     '''
     Simple simulated camera using Phong shading for pixels.
 
-    NOTE    that elements must implement `user_params={'diffuse': v, 'specular': s, 'n_s': n}`
-            where v is a Vector, s is a float, and n is a float
+    NOTE    elements must include `user_params={'specular': s, 'n_s': n}`
+            where s is a float and n is a float
     '''
     def __init__(self, width: int, height: int, position: Vector, pointing_direction: Vector):
         super().__init__(width, height, position, pointing_direction)
 
         # unilluminated pixels display this noise floor
-        self.lambertian_model_dark_term = Vector(0, 0, 0)
+        self.ambient_dark = Vector(0, 0, 0)
 
-    def _reflection_model(self, surface_normal_at_intersection, direction_to_source_unit, element, intersection_point, direction_to_origin_unit, intersection_point_illuminated):        
-        # lambertian shader model
-        # See Section 5.2, Real Time Rendering, Akenine-Moller et al., 2018
+    def _reflection_model(self, element, intersection_point, surface_normal_at_intersection, direction_to_origin_unit, direction_to_source_unit, intersection_point_illuminated):        
+        # lambertian diffuse shading. See Section 5.2, Real Time Rendering, Akenine-Moller et al., 2018
         dot_product_clamped = np.maximum(surface_normal_at_intersection.dot(direction_to_source_unit), 0)
-        lambertian_shading = element.surface_color(intersection_point) * dot_product_clamped * intersection_point_illuminated
-        lambertian_shader_model = self.lambertian_model_dark_term + lambertian_shading
+        lambertian_diffuse_shading = element.surface_color(intersection_point) * dot_product_clamped
 
-        # phong highlights
+        # blinn-phong spectral shading. See Section 33.1, Learning OpenGL - Graphics Programming, de Vries, 2020
         phong = np.clip(surface_normal_at_intersection.dot((direction_to_source_unit + direction_to_origin_unit).norm()), 0, 1)
-        phong_highlights = element.diffuse * element.specular * np.power(phong, element.n_s) * intersection_point_illuminated
-        return lambertian_shader_model + phong_highlights
+        phong_specular_shading = element.specular * np.power(phong, element.n_s)
+
+        return (self.ambient_dark + lambertian_diffuse_shading + phong_specular_shading) * intersection_point_illuminated
     
         # TODO support for scenes including multiple sources must be implemented HERE in the shader model. see Akenine-Moller.
 

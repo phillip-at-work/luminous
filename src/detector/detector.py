33240eb159b3d322535200e96f486f60975f132b
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-09-09 22:23:18 -0400

    updated Camera phong model with citations, syntax

--- src/detector/detector.py
+++ src/detector/detector.py
@@ -14,22 +14,21 @@ class Detector(ABC):
             height (float): Detector height in pixels
             pointing_direction (Vector): vector defining `Detector` pointing direction
         '''
+        self._data = None
         self.position = position
         self.width = width
         self.height = height
         self.pointing_direction = pointing_direction.norm()
 
     @abstractmethod
-    def _capture_data(self):
+    def _reflection_model(self) -> Vector:
         '''
-        Compile data for detector model
-        '''
-        pass
+        Recursively compile data for detector model
 
-    @abstractmethod
-    def _calculate_model(self):
-        '''
-        Perform calculations related to detector model
+        Support for multiple sources MUST be integrated here
+        e.g., shaded pixel values are the sum or weighted sum of the contributions of sources which illuminate that pixel
+
+        For a succinct description of reflection models, see 'Learning OpenGL - Graphics Programming' Chapter 6, 2020, de Vries
         '''
         pass
 
@@ -37,6 +36,8 @@ class Detector(ABC):
     def view_data(self):
         '''
         Perform any final processing and return data to user
+
+        Instance attribute `_data` should be accessed, manipulated if necessary, and returned
         '''
         pass
 
@@ -45,33 +46,39 @@ class PowerMeter(Detector):
     def __init__(self, width: int, height: int, position: Vector, pointing_direction: Vector):
         super().__init__(width, height, position, pointing_direction)
 
-    def _capture_data(self):
+    def _reflection_model(self):
         raise NotImplementedError(f"Method not yet implementated in {self.__class__}")
-    
-    def _calculate_model(self):
-        raise NotImplementedError(f"Method not yet implementated in {self.__class__}")
-    
+
     def view_data(self):
         raise NotImplementedError(f"Method not yet implementated in {self.__class__}")
 
 
 class Camera(Detector):
+    '''
+    Simple simulated camera using Phong shading for pixels.
+
+    NOTE    that elements must implement `user_params={'diffuse': v, 'specular': s, 'n_s': n}`
+            where v is a Vector, s is a float, and n is a float
+    '''
     def __init__(self, width: int, height: int, position: Vector, pointing_direction: Vector):
         super().__init__(width, height, position, pointing_direction)
-        self.data = Vector(0, 0, 0)
 
-    def _capture_data(self, surface_normal_at_intersection, direction_to_source_unit, element, intersection_point, intersection_point_illuminated):
-        # Ambient
-        color = Vector(0.05, 0.05, 0.05)
-        # Lambert shading (diffuse)
-        lv = np.maximum(surface_normal_at_intersection.dot(direction_to_source_unit), 0)
-        dc = element.diffuse_color(intersection_point) * lv * intersection_point_illuminated
-        return color + dc
+        # unilluminated pixels display this noise floor
+        self.lambertian_model_dark_term = Vector(0, 0, 0)
+
+    def _reflection_model(self, surface_normal_at_intersection, direction_to_source_unit, element, intersection_point, direction_to_origin_unit, intersection_point_illuminated):        
+        # lambertian shader model
+        # See Section 5.2, Real Time Rendering, Akenine-Moller et al., 2018
+        dot_product_clamped = np.maximum(surface_normal_at_intersection.dot(direction_to_source_unit), 0)
+        lambertian_shading = element.surface_color(intersection_point) * dot_product_clamped * intersection_point_illuminated
+        lambertian_shader_model = self.lambertian_model_dark_term + lambertian_shading
+
+        # phong highlights
+        phong = np.clip(surface_normal_at_intersection.dot((direction_to_source_unit + direction_to_origin_unit).norm()), 0, 1)
+        phong_highlights = element.diffuse * element.specular * np.power(phong, element.n_s) * intersection_point_illuminated
+        return lambertian_shader_model + phong_highlights
     
-    def _calculate_model(self, surface_normal_at_intersection, direction_to_source_unit, direction_to_origin_unit, intersection_point_illuminated):
-        # Blinn-Phong shading (specular)
-        phong = surface_normal_at_intersection.dot((direction_to_source_unit + direction_to_origin_unit).norm())
-        return Vector(1, 1, 1) * np.power(np.clip(phong, 0, 1), 50) * intersection_point_illuminated
+        # TODO support for scenes including multiple sources must be implemented HERE in the shader model. see Akenine-Moller.
 
     def view_data(self):
         '''
@@ -82,7 +89,7 @@ class Camera(Detector):
                 (255 * np.clip(c, 0, 1).reshape((self.height, self.width))).astype(np.uint8),
                 "L",
             )
-            for c in self.data.components()
+            for c in self._data.components()
         ]
 
         return Image.merge("RGB", rgb)

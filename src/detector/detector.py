9bdeb10bfff83d6c56279b306834197622c2bfd6
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-09-14 20:58:36 -0400

    updated documentation for existing Camera._reflection_model

--- src/detector/detector.py
+++ src/detector/detector.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 
 from ..math.vector import Vector
+from ..element.element import Element
 
 
 class Detector(ABC):
@@ -21,14 +22,26 @@ class Detector(ABC):
         self.pointing_direction = pointing_direction.norm()
 
     @abstractmethod
-    def _reflection_model(self) -> Vector:
+    def _reflection_model(  self, 
+                            element: Element,
+                            intersection_point: Vector,
+                            surface_normal_at_intersection: Vector, 
+                            direction_to_origin_unit: Vector, 
+                            intersection_map: list[dict]) -> Vector:
         '''
         Recursively compile data for detector model
 
-        Support for multiple sources MUST be integrated here
+        Support for multiple sources MUST be implemented here!
         e.g., shaded pixel values are the sum or weighted sum of the contributions of sources which illuminate that pixel
 
         For a succinct description of reflection models, see 'Learning OpenGL - Graphics Programming' Chapter 6, 2020, de Vries
+
+        Parameters:
+            element (Element): The element where the intersection occurs. Use this handle to access element-specific parameters needed for reflection model. Note that additional element parameters can be passed as key:value pairs using Element arg `user_params`.
+            intersection_point (Vector): Point of intersection
+            surface_normal_at_intersection (Vector): Normal vector with respect to element's surface at point of intersection
+            direction_to_origin_unit (Vector): Source direction for ray(s). Note that because rays are traced in reverse, these rays originate from the Detector (not the Source) or the previous reflection
+            intersection_map (list[dict]): A list of dicts representing how each source interacts with `intersection_point`. Each dict in the list contains the following keys: 'source' represents one Source object, 'direction_to_source_unit' is the Vector direction from `intersection_point` to Source, 'intersection_point_illuminated' is a numpy array of booleans indicating if corresponding rays are illuminated by that Source, else, are in a shadow
         '''
         pass
 
@@ -66,18 +79,26 @@ class Camera(Detector):
         # unilluminated pixels display this noise floor
         self.ambient_dark = Vector(0, 0, 0)
 
-    def _reflection_model(self, element, intersection_point, surface_normal_at_intersection, direction_to_origin_unit, direction_to_source_unit, intersection_point_illuminated):        
-        # lambertian diffuse shading. See Section 5.2, Real Time Rendering, Akenine-Moller et al., 2018
-        dot_product_clamped = np.maximum(surface_normal_at_intersection.dot(direction_to_source_unit), 0)
-        lambertian_diffuse_shading = element.surface_color(intersection_point) * dot_product_clamped
+    def _reflection_model(self, element, intersection_point, surface_normal_at_intersection, direction_to_origin_unit, intersection_map):
 
-        # blinn-phong spectral shading. See Section 33.1, Learning OpenGL - Graphics Programming, de Vries, 2020
-        phong = np.clip(surface_normal_at_intersection.dot((direction_to_source_unit + direction_to_origin_unit).norm()), 0, 1)
-        phong_specular_shading = element.specular * np.power(phong, element.n_s)
+        s = Vector(0,0,0)
 
-        return (self.ambient_dark + lambertian_diffuse_shading + phong_specular_shading) * intersection_point_illuminated
-    
-        # TODO support for scenes including multiple sources must be implemented HERE in the shader model. see Akenine-Moller.
+        for v in intersection_map:
+
+            direction_to_source_unit = v['direction_to_source_unit']
+            intersection_point_illuminated = v['intersection_point_illuminated']
+
+            # lambertian diffuse shading. See Section 5.2, Real Time Rendering, Akenine-Moller et al., 2018
+            dot_product_clamped = np.maximum(surface_normal_at_intersection.dot(direction_to_source_unit), 0)
+            lambertian_diffuse_shading = element.surface_color(intersection_point) * dot_product_clamped
+
+            # blinn-phong spectral shading. See Section 33.1, Learning OpenGL - Graphics Programming, de Vries, 2020
+            phong = np.clip(surface_normal_at_intersection.dot((direction_to_source_unit + direction_to_origin_unit).norm()), 0, 1)
+            phong_specular_shading = element.specular * np.power(phong, element.n_s)
+
+            s += (lambertian_diffuse_shading + phong_specular_shading) * intersection_point_illuminated
+
+        return self.ambient_dark + s
 
     def view_data(self):
         '''
